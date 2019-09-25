0fa24f06a99f4328a2bae9023943de7a866302c6
Marina <kravchenko.marite@gmail.com>
2019-09-25 22:28:48 +0300

    сделано 3 дз по видео уроку

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3,
+    speed: 5,
     traffic: 3
 };
 
@@ -31,6 +31,10 @@ function getQuantityElements(heightElement) {
 
 function startGame(){
     start.classList.add('hide');
+    gameArea.innerHTML = '';
+    car.style.left = '125px';
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -50,8 +54,12 @@ function startGame(){
 
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -60,6 +68,8 @@ function startGame(){
 function playGame(){
    
     if (setting.start){
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE<br>' + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
@@ -110,6 +120,18 @@ function moveRoad() {
 function moveEnemy(){
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item){
+   let carRect = car.getBoundingClientRect();
+   let enemyRect = item.getBoundingClientRect();
+
+   if (carRect.top <= enemyRect.bottom && 
+    carRect.right >= enemyRect.left && 
+    carRect.left <= enemyRect.right &&
+    carRect.bottom >= enemyRect.top){
+       setting.start = false; 
+       console.warn('ДТП');
+       start.classList.remove('hide');
+       start.style.top = score.offsetHeight;
+   }
    item.y += setting.speed / 2;
    item.style.top = item.y + 'px';
 
